340e0c210a805abefce24cf8e70ed5e1402d0703
alpinist-453 <alpinist_453@mail.ru>
2022-02-08 13:17:55 +0300

    первая задача выполнена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,40 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b**2-4*a*c;
+  
+
+  if (d < 0) {
+
+  } else if(d === 0) {
+      let x = -b/(2*a);
+      arr.push(x);
+  } else if(d > 0 ) {
+      let x1 = (-b + Math.sqrt(d) )/(2*a);
+      arr.push(x1);
+      let x2 = (-b - Math.sqrt(d) )/(2*a);
+      arr.push(x2);
+  }
+
   return arr; // array
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
