bd399090527bb0acb6f7ccaf2cb8fde24ad7948c
alpinist-453 <alpinist_453@mail.ru>
2022-02-15 00:23:41 +0300

    Все 3 задача готовы на проверку

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,67 @@
 // Задание 1
+
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
 
+    sum = sum + arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
+    
   return { min: min, max: max, avg: avg };
 }
 
-// Задание 2
+
+//--------------------------------------------------------------------------------- Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let initialValue = 0;
+  let sum = arr.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+  let max = 0;
   
+  for ( let i = 0; i < arrOfArr.length; i++) {
+    let rezalt = func(arrOfArr[i]);
+       if (max < rezalt) {
+      max = rezalt;
+    }
+  }
   return max;
 }
 
-// Задание 3
+// ---------------------------------------------------------------------------------- Задание 3
+
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let diff = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    diff = Math.abs(max - min);
+
+  }
+
+  return diff;
 }
+
