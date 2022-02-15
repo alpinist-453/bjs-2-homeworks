91bd284edb2c001aa8d2fd50723e95cb09f5e746
alpinist-453 <alpinist_453@mail.ru>
2022-02-15 16:49:19 +0300

    Готовы к сдаче обе задачи.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
+
+// ----------------- задача 1 -------------------------//
 function compareArrays(arr1, arr2) {
-  let result;
+	let result = 0;
 
-  // Ваш код
+	result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
-  return result; // boolean
+	return result; // boolean
 }
 
+// ----------------- задача 2 -------------------------//
+
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+	let resultArr = 0;
 
-  return resultArr; // array
+	return resultArr = arr.filter(ithem => ithem > 0).filter(krat => krat % 3 === 0).map((num) => num * 10);
 }
