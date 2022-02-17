4b5e389ec7b700e773b792f8502184ed8752467e
alpinist-453 <alpinist_453@mail.ru>
2022-02-17 19:02:09 +0300

    Задача по объектам готова для проверки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
+
+const student1 = new Student("Maxim", "man", "36");
+const student2 = new Student("Konstantin", "man", "34");
+const student3 = new Student("Elena", "male", "32");
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+	this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+	if (this.marks === undefined) {
+		this.marks = [];
+		this.marks.push(mark)
+	} else {
+		this.marks.push(mark)
+	}
+}
+
+Student.prototype.addMarks = function (mark1, mark2, mark3, ...marks) {
+	if (this.marks === undefined) {
+		this.marks = [];
+		this.marks.push(mark1, mark2, mark3, ...marks)
+	} else {
+		this.marks.push(mark1, mark2, mark3, ...marks)
+	}
+}
+
+Student.prototype.getAverage = function (arrayMark) {
+	
+	 return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
+}
